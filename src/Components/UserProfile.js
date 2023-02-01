f63ebc921d1e4753a5767f9493642a9d95ad7eb6
SuperNinjaEv <evan0210@gmail.com>
2023-01-31 21:58:00 -0500

    Finished lab, all tests passing, added some extra styling

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,12 @@
+function UserProfile(props) {
+
+    return (
+        <div class="user">
+            <h3>Evan Velush</h3>
+            <p><strong>React Creator</strong></p>
+            <p>Lorem Ipsum</p>
+        </div>
+    )
+}
+
+export default UserProfile;
