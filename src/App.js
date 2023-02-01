f9499a2e2dbc5698d6d55e0550c71d814aa48846
MaxWattan <mcwattanac@gmail.com>
2023-01-31 20:22:34 -0500

    got test to pass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Contacts from "./Components/Contacts";
+// import ContactUserCard from "./Components/ContactUserCard";
+import NavBar from "./Components/NavBar";
+import UserProfile from "./Components/UserProfile";
 import "./index.css";
+// import Post from "./Components/Post";
+import Posts from "./Components/Posts";
+
+
 
 function App () {
-  return <p>Hello, world!</p>;
+  return (
+    <div>
+  <p>Hello, world!</p>
+
+  <NavBar />
+  <UserProfile />
+  <Posts />
+  <Contacts />
+  {/* <Post /> */}
+  {/* <ContactUserCard /> */}
+
+  </div>
+  )
 };
 
 export default App;
---
+++ src/Components/Post.js
@@ -0,0 +1,10 @@
+function Post () {
+    return (
+        <div>
+           <h2>Title</h2>
+           <h3> Lorem Ipsom</h3>
+        </div>
+        
+    )
+}
+export default Post;
