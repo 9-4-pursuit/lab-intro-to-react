667733a37597195c4d19d8341291e2fc3727d6ac
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-02-01 16:44:46 -0500

    added buttons to NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,10 @@
 const NavBar = () => {
     return (
         <div className="NavBar">
-        <p>NavBar</p>
+        {/* <p>NavBar</p> */}
+        <button>What is Pursuit?</button>
+        <button>Create an account</button>
+        <button>Sign in</button>
         </div>
     )
 }
