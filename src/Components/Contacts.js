85b295b7d6f20375e59e03b01928ce28deed9ef3
keriarmstrong <khetto@live.com>
2023-01-31 20:02:05 -0500

    intro-to-react

---
+++ src/Components/Contacts.js
@@ -0,0 +1,15 @@
+import ContactUserCard from "./ContactUserCard"
+
+function Contacts() {
+    return(
+        <div className="Contacts">
+        <ContactUserCard />
+        <ContactUserCard />
+        <ContactUserCard />
+        <ContactUserCard />
+
+        </div>
+     
+    )
+}
+export default Contacts
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,13 @@
+function UserProfile() {
+
+return (
+    <div className ="ContactUserCard">
+      <img src="./profile.png" alt="profile image"></img>
+        <h2> Jordan Walke</h2>
+        <h3> React Creator</h3>
+        <p> lorem</p>
+
+    </div>
+)
+}
+export default UserProfile
