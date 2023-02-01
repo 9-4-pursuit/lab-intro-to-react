6ab54abac61f04b4f54946bf73e7b439d1547dc8
Ari-So-Irie <arius1500@gmail.com>
2023-02-01 05:22:27 -0500

    Arius's Intro To React Lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./index.css";
-
-function App () {
-  return <p>Hello, world!</p>;
-};
+import Posts from "./Components/Posts";
+import NavBar from "./Components/NavBar";
+import Contacts from "./Components/Contacts";
+import UserProfile from "./Components/UserProfile";
+function App() {
+  return (
+    <body>
+      <header class="nav">
+        <NavBar />
+      </header>
+      <div class="profile">
+        <UserProfile />
+      </div>
+      <div class="posts">
+        <Posts />
+      </div>
+      <aside class="contacts">
+        <Contacts />
+      </aside>
+    </body>
+  );
+}
 
 export default App;
---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,11 @@
+function ContactUserCard() {
+  return (
+    <div class="users">
+      <img src="profile.png" />
+      <h3>Jordan Walke</h3>
+      <h4>React Creator</h4>
+      <p>Lorem Ipsem</p>
+    </div>
+  );
+}
+export default ContactUserCard;
