22810e69d6c7d1dfdaf1a7bce6e73be5630a90d8
Angel <EMAIL>
2023-01-31 21:47:53 -0500

    lab 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import NavBar from './Components/NavBar';
+import UserProfile from "./Components/UserProfile";
+import Contacts from "./Components/Contacts";
+import Posts from "./Components/Posts";
 import "./index.css";
 
-function App () {
-  return <p>Hello, world!</p>;
-};
+const App = () => {
+   
+  return (
+    <div>
+      <NavBar />
+      <UserProfile />
+      <Contacts />
+      <Posts />
+    </div>
+  )
+}
 
 export default App;
