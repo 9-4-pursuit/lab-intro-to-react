8e207aca115efac38209e4f23e54960637ae7ecc
RadiantRay22 <rayrussell@pursuit.org>
2023-02-15 20:02:56 -0500

    Ray is Done

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,24 @@
+
+// When naming the function, you must name the function exactly as component
+
+/* The template function is
+function NameofCompenent () {
+ return (
+    inside here your create a tag <div></div> or <> </>
+    inside of there we create the elements we want to see on the 
+    webpage
+ )
+} */
+
+
+function ContactUserCard() {
+    return (
+        <div>
+            <h3>Jordan Walke</h3>
+            <p><strong>React Creator</strong></p>
+            <p>Lorem ipsum</p>
+        </div>
+    )
+}
+
+export default ContactUserCard
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+
+function NavBar() {
+
+    return (
+        <div>
+            <button>What is Pursuit?</button>
+            <button>Create an account</button>
+            <button>Sign In</button>
+        </div>
+    )
+
+}
+
+export default NavBar;
